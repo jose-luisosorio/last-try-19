a37fcb4e5b9b208729b6df27a1a282e997dda1ec
jose-luisosorio <joseosorio@berkeley.edu>
2024-02-11 18:26:09 -0500

    2/11 Changes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 console.log("Your index.js  file is loaded correctly!");
+function openSection(evt, sectionName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("section");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    tablinks = document.getElementsByClassName("tablink"); 
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("w3-red", "");
+    }
+    document.getElementById(sectionName).style.display = "block";
+    evt.currentTarget.className += "w3-red";  
+  }
